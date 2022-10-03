fd095c157c66b516c88ea0252e0fa02a01725270
Marcelo Ramirez <ramirezyapura@gmail.com>
2022-10-03 20:53:34 -0300

    borrado de comentarios

--- ejercicio-4/ejercicio-4.js
+++ ejercicio-4/ejercicio-4.js
@@ -1,48 +1,8 @@
 /* 3. Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -. */
 
-/* let value = prompt("ingresa algo");
-let value2;
-while(typeof value != typeof value2){
-    value = value + "-";
-    value2 = prompt("ingresa algo");
-    
-} */
-/* 
-let valor1 = prompt("ingresa algo");
-let result;
+let nadia = prompt("ingresa un valor");
+let resultado;
 
-while(valor1 != null){
-    if(valor1 && result){
-        valor1 = prompt("ingresa algo");
-        result = result + valor1 + "-";
-    }else if(valor1 != null){
-        result = valor1 + "-";
-    }
-};
-document.write(result) */
-
-/* let uno = null, dos = "-";
-document.write(uno + dos) */
-
-/* let string, resultado;
-
-while(string != null){
-    string = prompt("ingresa algo");
-    resultado = resultado + string + "-";
-};
-document.write(resultado) */
-let string, resultado;
-document.write(typeof string != null)
-
-
-
-
-
-
-
-
-
-
-
-
-
+do{
+    prompt
+}
