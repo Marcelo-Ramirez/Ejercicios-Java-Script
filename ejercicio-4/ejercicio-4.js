2c4cabec43b5c2bc07d506eac90b4488e2d85442
Marcelo Ramirez <ramirezyapura@gmail.com>
2022-10-04 10:43:28 -0400

    comentarios probando consola

--- ejercicio-4/ejercicio-4.js
+++ ejercicio-4/ejercicio-4.js
@@ -1,2 +1,14 @@
 /* 3. Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -. */
 
+let valor1 = 3;
+let result;
+
+/* while (valor1 != null){
+    if (valor1 && result){
+        
+    }else{
+        result += valor1 + "-";
+    }  
+} */
+
+console.log(true && valor1);
