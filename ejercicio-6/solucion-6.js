0568a910ceee92d687830c0caef1b1b0d2e030aa
Marcelo Ramirez <ramirezyapura@gmail.com>
2022-10-14 00:30:34 -0400

    agregando ejercicio 6 solucionado por mi

---
+++ ejercicio-6/solucion-6.js
@@ -0,0 +1,18 @@
+/* 9. Escribe un programa que pida una frase y escriba cuantas veces aparece la letra a. */
+
+let company = prompt("Ingresa una palabra"), variante, suma = 0;
+
+while(true){
+    variante = company
+    company = company.substr(company.indexOf("a") + 1);
+    if(variante != company){
+        suma++
+    }
+    if (variante == company){
+    break;
+    }
+}
+document.write("la letra (a) aparece " + suma + " veces")
+
+
+
