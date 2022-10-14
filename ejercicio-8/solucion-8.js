b224433a6d6171ccf30387a2c14b699ef6c180f8
Marcelo Ramirez <ramirezyapura@gmail.com>
2022-10-14 19:30:22 -0400

    ejercicio 8 TER

--- ejercicio-8/solucion-8.js
+++ ejercicio-8/solucion-8.js
@@ -1,1 +1,13 @@
 /* Escribe un programa que pida una frase y escriba cuántas de las letras que tiene son vocales.*/
+let palabra = prompt("ingresa texto"), vocales, value = 0, num = 0, result = 0;
+
+vocales = ["a","e","i","o","u"]
+while(true){
+    result = result + value;
+    if (num == 5){
+        break;
+    }
+    value = palabra.split(vocales[num]).length -1;
+    num++
+}
+document.write("tu palabra tiene " + result + " vocales")
