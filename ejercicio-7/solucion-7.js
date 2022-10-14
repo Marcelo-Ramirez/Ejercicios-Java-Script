73b2c37360405301081c253ad32de4145183ba7c
Marcelo Ramirez <ramirezyapura@gmail.com>
2022-10-14 11:06:47 -0400

    ejercicio 7 ya casi terminado

--- ejercicio-7/solucion-7.js
+++ ejercicio-7/solucion-7.js
@@ -1,4 +1,14 @@
 /* 10. Escribe un programa que pida una frase y escriba las vocales que aparecen. */
 
-let text = "vocabulario";
+let text = "vocabulario", numero = 0, num;
 
+let vocales = ["a","e","i","o","u"];
+
+while(true){
+    if (numero == 5){
+        break;
+    }
+    document.write(num = text.split(vocales[numero]).length -1 + "<br>");
+    document.write("Letra " +"("+ vocales[numero] + ") " + num + "veces" + "<br>")
+    numero++;
+}
