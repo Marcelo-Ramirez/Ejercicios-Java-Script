172607e7db3857dd5461692648d281d0132eb0b4
Marcelo Ramirez <ramirezyapura@gmail.com>
2022-10-14 10:25:32 -0400

    agregando otra solucion al ejercicio 6

---
+++ ejercicio-6/otra-solucion.js
@@ -0,0 +1,5 @@
+/* Escribe un programa que pida una frase y escriba cuantas veces aparece la letra a.*/
+
+let str = "auditoria", num;
+num = str.split("a").length -1
+console.log("Esa palabra tiene " + num + " veces (a)") // Esa palabra tiene 2 veces (a)
