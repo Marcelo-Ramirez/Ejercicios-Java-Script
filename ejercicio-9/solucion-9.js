5e62acbd1d98602ddb5b34a27ef9a9d30bf95c15
Marcelo Ramirez <ramirezyapura@gmail.com>
2022-10-14 20:39:42 -0400

    ejercicio 9 ya disponible para resolver

--- ejercicio-9/solucion-9.js
+++ ejercicio-9/solucion-9.js
@@ -1,9 +1,3 @@
-El caracol sube 7 pies cada día y retrocede 2 pies cada noche. ¿Cuántos días le tomará al caracol salir de un pozo con la profundidad dada?
-
-Ejemplo de entrada:
-
-31
-
-Ejemplo de salida:
-
-6
+/* El caracol sube 7 pies cada día y retrocede 2 pies cada noche. ¿Cuántos días le tomará al caracol salir de un pozo con la profundidad dada? 
+Ejemplo:
+Input 31, ouput 6 */
