f52f154d4edb8ae3e7b099fb322406a2f534b852
Marcelo Ramirez <ramirezyapura@gmail.com>
2022-10-20 22:15:04 -0400

    agregando mi solucion al ejercicio 1

--- ejercicio-1/ejercicio-1.js
+++ ejercicio-1/ejercicio-1.js
@@ -1,1 +1,9 @@
 /* Escriba un programa que pida dos numeros y te devuelva el mayor valor */
+let value1 = Number(prompt("primer valor"));
+let value2 = Number(prompt("segundo valor"));
+
+if (value1 > value2){
+    alert(value1 + " es el mayor");
+}else{
+    alert(value2 + " es el mayor");
+}
