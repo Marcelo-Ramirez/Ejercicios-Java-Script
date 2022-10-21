27f57ea0f435f345bd2167fc6a2bbacc865b4cc4
Marcelo Ramirez <ramirezyapura@gmail.com>
2022-10-21 08:15:07 -0400

    ejercicio 2 solucionado ... el codigo no esta tan pulido

--- ejercicio-2/ejercicio-2.js
+++ ejercicio-2/ejercicio-2.js
@@ -7,3 +7,33 @@
 7-9: Notable
 9-10: Sobresaliente */
 
+let value = Number(prompt("ingresa un numero de 0 al 10"));
+if (value >= 0 && value <= 10){
+    switch (value){
+        case 0:
+        case 1:
+        case 2:
+            alert("muy deficiente");
+        break;
+        case 3:
+        case 4:
+            alert("insuficiente");
+        break;
+        case 5:
+            alert("sufieciente");
+        break;
+        case 6:
+            alert("bien");
+        break;
+        case 7:
+        case 8:
+            alert("notable");
+        break;
+        case 9:
+        case 10:
+            alert("sobresaliente");
+        break;
+    }
+}else{
+    alert("el numero no es valido");
+}
